948d337e676a8c51300f00f914f8eff47097dff1
lornico14 <nikolaslorenzo@gmail.com>
2023-09-19 11:47:17 -0300

    Agrego archivo SQL y Modifico README.md

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # TPE-WEB2
 Trabajo Práctico Especial de Web 2
+
+Integrantes:
+    -RIVA, Franco
+    -LORENZO, Nicolas
+
+El sitio permite ver a los visitantes info detallada sobre las diferentes placas de video a la venta, incluyendo especificaciones tecnicas, los administradores podran agregar, quitar y editar las placas de video y administrar las especificaciones asociadas a ellas.
+
+Creamos dos tablas, "Producto" para almacenar la informacion sobre las placas, y "Especificacion" para almacenar las especificaciones tecnicas relacionadas con cada placa de video.
+La clave primaria de la tabla "Producto" es "ID", y la tabla "Especificacion" tiene una clave primaria "ID" y una clave foránea "Producto_ID" que se relaciona con el campo "ID" de la tabla "Producto".
+
+Nuestro modelo de datos permite la relacion 1:N Entre productos y especificaciones, lo que significa que cada placa de video puede tener multiples especificaciones asociadas.
+
+
+Diagrama de Entidad relacion:
+
+Producto (Placa de Video):
+
+    -Atributos:
+        ID (Clave Primaria)
+        Marca
+        Modelo
+        Descripción
+        Precio
+    -Relaciones:
+        Una Placa de Video tiene muchas Especificaciones (relación 1:N)
+    
+
+Especificación:
+
+    -Atributos:
+        ID (Clave Primaria)
+        Marca
+        Modelo
+        Valor
+        Memoria
+        GPU Clock
+        Memory Clock
+        Fabricante
+        
+    -Relaciones:
+        Pertenecerá a una Placa de Video (relación N:1)
+
+Exportamos el archivo placas_de_video desde phpMyAdmin en formato .sql.
+
---
+++ placas_de_video.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-09-2023 a las 16:37:28
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `placas_de_video`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `especificacion`
+--
+
+CREATE TABLE `especificacion` (
+  `ID` int(11) NOT NULL,
+  `Marca` varchar(255) NOT NULL,
+  `Modelo` varchar(255) NOT NULL,
+  `Valor` varchar(255) NOT NULL,
+  `Memoria` int(11) NOT NULL,
+  `GPU_Clock` int(11) NOT NULL,
+  `Memory_Clock` int(11) NOT NULL,
+  `Fabricante` varchar(255) DEFAULT NULL,
+  `Producto_ID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `ID` int(11) NOT NULL,
+  `Marca` varchar(255) NOT NULL,
+  `Modelo` varchar(255) NOT NULL,
+  `Descripcion` text DEFAULT NULL,
+  `Precio` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `especificacion`
+--
+ALTER TABLE `especificacion`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `Producto_ID` (`Producto_ID`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `especificacion`
+--
+ALTER TABLE `especificacion`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `especificacion`
+--
+ALTER TABLE `especificacion`
+  ADD CONSTRAINT `especificacion_ibfk_1` FOREIGN KEY (`Producto_ID`) REFERENCES `producto` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

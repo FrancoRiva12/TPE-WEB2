4b2975155f2e1a5f80c4e2f95c32bb3e09502bee
lornico14 <nikolaslorenzo@gmail.com>
2023-10-11 17:23:21 -0300

    login

--- login.php
+++ login.php
@@ -1,37 +1,30 @@
 <?php
-session_start();
-
-// Verificar si el usuario ya está autenticado, en cuyo caso redirigirlo a otra página.
-if (isset($_SESSION['usuario'])) {
-    header("Location: pagina_de_inicio.php");
-    exit;
-}
-
-// Conectar a la base de datos
-require 'config.php'; // Aseguremonos de que este archivo contenga la confign de la base de datos
-
+// Verificar si el usuario ha enviado el formulario
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Recuperar el nombre de usuario y contraseña del formulario
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    //verifico las credenciales del usuario
-    $sql = "SELECT * FROM Usuarios WHERE Username = :username";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-    $stmt->execute();
+    // Consulta para verificar las credenciales del usuario
+    $query = $pdo->prepare('SELECT * FROM Usuarios WHERE Username = ?');
+    $query->execute([$username]);
+    $user = $query->fetch(PDO::FETCH_OBJ);
 
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    // Verificar la contraseña
+    if ($user && password_verify($password, $user->Password)) {
+        // Iniciar sesión
+        session_start();
+        $_SESSION['user_id'] = $user->ID;
+        $_SESSION['username'] = $user->Username;
 
-    if ($user && password_verify($password, $user['Password'])) {
-        // Iniciar sesión y redirigir al usuario a la página de inicio
-        $_SESSION['usuario'] = $user;
-        header("Location: pagina_de_inicio.php");
-        exit;
+        // redirect a la pagina principal
+        header('Location: mainPage.php');
+        exit();
     } else {
-        $error = "Credenciales inválidas. Inténtalo de nuevo.";
+        // Error si erramos contraseña o usuario
+        $error_message = "Credenciales incorrectas.";
     }
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -40,20 +33,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Iniciar Sesión</title>
 </head>
 <body>
-    <h1>Iniciar Sesión</h1>
 
-    <?php if (isset($error)) { ?>
-        <p style="color: red;"><?php echo $error; ?></p>
-    <?php } ?>
+<h2>Iniciar Sesión</h2>
 
-    <form method="post">
-        <label for="username">Usuario:</label>
-        <input type="text" name="username" id="username" required><br>
+<?php
+if (isset($error_message)) {
+    echo "<p>$error_message</p>";
+}
+?>
 
-        <label for="password">Contraseña:</label>
-        <input type="password" name="password" id="password" required><br>
+<form method="POST" action="login.php">
+    <label for="username">Usuario:</label>
+    <input type="text" id="username" name="username" required><br><br>
+    
+    <label for="password">Contraseña:</label>
+    <input type="password" id="password" name="password" required><br><br>
+    
+    <button type="submit">Iniciar Sesión</button>
+</form>
 
-        <button type="submit">Iniciar Sesión</button>
-    </form>
 </body>
 </html>
